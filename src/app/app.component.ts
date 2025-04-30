6932fdcae334b8ad7867586843a86631e093b9d7
kannanATexcelforte <kannan@excelforte.com>
2025-04-30 07:03:14 +0530

    30.04.2025
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'jvl-code';
+  prevTitle = 'Hello';
+
+  ngDoCheck(): void {
+    if (this.title !== this.prevTitle) {
+      console.log('property changed', this.title);
+    }
+  }
 }
