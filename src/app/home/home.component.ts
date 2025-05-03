a02aa9bde8a9b1540c0142c5f87a155fe023c22f
kannanATexcelforte <kannan@excelforte.com>
2025-05-03 13:07:33 +0530

    03.05.2025
    completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private router: Router) { }
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
 }
